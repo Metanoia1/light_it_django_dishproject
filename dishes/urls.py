148db0acf52b64c5a327ad1a3f4c740b901ef45c
metanoia <meta3479@gmail.com>
2021-07-20 13:49:31 +0300

    version 1.2

--- dishes/urls.py
+++ dishes/urls.py
@@ -7,7 +7,7 @@ app_name = "dishes"
 
 urlpatterns = [
     path("", views.DishList.as_view(), name="index"),
-    path("<int:pk>/", views.DishDetail.as_view(), name="details"),
+    path("<int:pk>", views.DishDetail.as_view(), name="details"),
     path("orders/", views.OrderList.as_view(), name="orders"),
     path("order/<int:dish_id>", views.create_order, name="order"),
 ]
