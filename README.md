61e9c5c2b2950f0504c20b2dacd02c2fb1c613c5
metanoia <meta3479@gmail.com>
2021-08-20 15:48:06 +0300

    staticfiles_urlpatterns added

--- README.md
+++ README.md
@@ -41,4 +41,4 @@ docker build -f Dockerfile --tag app:latest .
 docker run -d -p 8082:8000 app
 
 ```
-[127.0.0.1:8082](http://127.0.0.1:8082)
+[127.0.0.1:8082/admin](http://127.0.0.1:8082/admin/)
--- dishproject/urls.py
+++ dishproject/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
+
 from rest_framework import permissions
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
@@ -74,6 +77,8 @@ urlpatterns = [
         "api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"
     ),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     import debug_toolbar
