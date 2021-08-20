58259d06c8618237b694f34c720b1b353f2ed521
metanoia <meta3479@gmail.com>
2021-08-20 17:29:50 +0300

    api/services.py refactored

--- api/services.py
+++ api/services.py
@@ -36,13 +36,8 @@ class DishModelService:
         return Response(data, status=status.HTTP_201_CREATED)
 
     def get_top_dishes(self, top_number):
-        my_cache = caches["db_cache"]
-        dishes = my_cache.get("dishes_list", [])
-        if not dishes:
-            dishes = (
-                Dish.objects.all()
-                .annotate(orders_count=Count("orders"))
-                .order_by("-orders_count")[:top_number]
-            )
-            my_cache.set("dishes_list", dishes, 120)
-        return dishes
+        return (
+            Dish.objects.all()
+            .annotate(orders_count=Count("orders"))
+            .order_by("-orders_count")[:top_number]
+        )
